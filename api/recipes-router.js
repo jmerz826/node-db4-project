055426e94fb578a51aebb00131b31677c4c07548
John Merz <jmerz826@gmail.com>
2022-01-20 22:58:01 -0500

    can fetch by id

--- api/recipes-router.js
+++ api/recipes-router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
+const Recipe = require('./recipes-model')
 
 router.get('/:id', (req, res, next) => {
-    
+    Recipe.findById(req.params.id)
+        .then(rec => {
+            res.status(200).json(rec)
+        })
+        .catch(err => next(err))
 })
 
 module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,7 +1,17 @@
 const express = require('express')
+const recipesRouter = require('./recipes-router')
 
 const server = express()
 
 server.use(express.json())
+
+server.use('/api/recipes', recipesRouter)
+
+server.use('/', (err, req, res, next) => {
+    res.status(500).json({
+        message: `request ${req.method} could not be completed`,
+        stack: err.stack
+    })
+})
 
 module.exports = server
